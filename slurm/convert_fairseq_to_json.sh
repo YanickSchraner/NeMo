7e3f53c019fdd99244ec732b748a79499fde5947
Yanick Schraner <yanick.schraner@fhnw.ch>
2023-03-06 09:24:47 +0100

    Convert fairseq data to nemo json manifest

--- slurm/convert_fairseq_to_json.sh
+++ slurm/convert_fairseq_to_json.sh
@@ -12,4 +12,4 @@ ml Miniconda2/4.3.30
 ml intel/2022.00
 source activate nemo
 
-sbatch python scripts/dataset_processing/fairseq_to_json.py --data_dir '/scicore/home/graber0001/GROUP/stt/swissgerman_wav2vec_nobackup/data/corpus/stt/annotated/combined_annotated_speech/2022-10-21_12-29-58' --splits 'train,test,valid' --manifest '/scicore/home/graber0001/GROUP/stt/nemo_nobackup/data/combined/2022-10-21_12-29-58'
+python scripts/dataset_processing/fairseq_to_json.py --data_dir '/scicore/home/graber0001/GROUP/stt/swissgerman_wav2vec_nobackup/data/corpus/stt/annotated/combined_annotated_speech/2022-10-21_12-29-58' --splits 'train,test,valid' --manifest '/scicore/home/graber0001/GROUP/stt/nemo_nobackup/data/combined/2022-10-21_12-29-58'
